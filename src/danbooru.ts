425a53c931bc46b10864b7384b39f28fab301952
Shigma <shigma10826@gmail.com>
2022-07-27 03:05:28 +0800

    build: setup build workflow

--- src/danbooru.ts
+++ src/danbooru.ts
@@ -1,8 +1,8 @@
 import { Session } from 'koishi'
-import Cheerio from 'cheerio'
+import { load } from 'cheerio'
 
 export default async function danbooru(url: string, session: Session) {
   const data = await session.app.http.get(url)
-  const $ = Cheerio.load(data)
+  const $ = load(data)
   return $('#image-container').attr('data-normalized-source')
 }
